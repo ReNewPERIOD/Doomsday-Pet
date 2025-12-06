9cffed6be40a688a1bc171815e87e689c2721c05
ReNewPERIOD <ngotranthanhhuy83@gmail.com>
2025-12-06 12:01:51 +0700

    loan xa final2

--- doomsday-pet/app/page.js
+++ doomsday-pet/app/page.js
@@ -10,7 +10,7 @@ import "@solana/wallet-adapter-react-ui/styles.css";
 
 // --- CẤU HÌNH ---
 const PROGRAM_ID = new PublicKey("CrwC7ekPmUmmuQPutMzBXqQ4MTydjw1EVS2Zs3wpk9fc");
-const GAME_ADDRESS = new PublicKey("Hxj8rKhDudPr97PhUjK1MVgohis9PJFUNsB2PCVwb3qU");
+const GAME_ADDRESS = new PublicKey("BzhTWXrqmZAkUcjJRhxsMsEKpCNbYQcjKdFCqwyzhf4D");
 
 // --- URL VIDEO ---
 const VIDEO_NORMAL   = "/v1.mp4"; 
