b232e76d55743ab48151719d5ff0f3d9542f3983
ReNewPERIOD <ngotranthanhhuy83@gmail.com>
2025-12-06 20:40:36 +0700

    change game address to fix 6001

--- doomsday-pet/app/page.js
+++ doomsday-pet/app/page.js
@@ -10,7 +10,7 @@ import "@solana/wallet-adapter-react-ui/styles.css";
 
 // --- CẤU HÌNH ---
 const PROGRAM_ID = new PublicKey("CrwC7ekPmUmmuQPutMzBXqQ4MTydjw1EVS2Zs3wpk9fc");
-const GAME_ADDRESS = new PublicKey("9d83LmoQT6LrWa4NDpStzQByA96fTRShnWGqESMYBJu8");
+const GAME_ADDRESS = new PublicKey("HQoSwQhBYxCgL4tJeaBRtf4XBTGeVJJdPQ8v33s9czRT");
 
 // --- URL VIDEO ---
 const VIDEO_NORMAL   = "/v1.mp4"; 
@@ -356,3 +356,4 @@ export default function Home() {
     </ConnectionProvider>
   );
 }
+// test update
